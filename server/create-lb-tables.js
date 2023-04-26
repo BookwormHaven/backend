d799723c5e561d1aa148a7f18d9f9874ae2926e3
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-04-26 18:25:33 +0800

    setup author and book models

---
+++ server/create-lb-tables.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var server = require('./server');
+var ds = server.dataSources.pgsql_db;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Book', 'Author'];
+
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
