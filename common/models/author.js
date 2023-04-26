2f9363a1b694cf616aeb874fd433e2877fb0c44a
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-04-26 22:27:54 +0800

    add model validation

--- common/models/author.js
+++ common/models/author.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Author) {
+  Author.validatesLengthOf('name', {
+    min: 2,
+    max: 128,
+    message: {
+      min: 'Author name should be at least 2 characters.',
+      max: 'Author name should be at most 128 characters.',
+    },
+  });
 
+  Author.validatesLengthOf('biography', {
+    min: 16,
+    max: 256,
+    message: {
+      min: 'Author biography should be at least 16 characters.',
+      max: 'Author biography should be at most 256 characters.',
+    },
+    allowBlank: true,
+    allowNull: true,
+  });
 };
--- common/models/book.js
+++ common/models/book.js
@@ -1,5 +1,36 @@
 'use strict';
 
 module.exports = function(Book) {
+  function bookYearValidator(err) {
+    const {year} = this;
+    if (typeof year !== 'number' ||
+      year < 1 ||
+      year > new Date().getFullYear())
+      err();
+  };
 
+  Book.validate('year', bookYearValidator, {
+    allowNull: false,
+    message: 'Invalid year entered. Year should be a positive integer and should not be in the future',
+  });
+
+  Book.validatesLengthOf('title', {
+    min: 2,
+    max: 128,
+    message: {
+      min: 'Book title should be at least 2 characters.',
+      max: 'Book title should be at most 128 characters.',
+    },
+  });
+
+  Book.validatesLengthOf('publisher', {
+    min: 2,
+    max: 128,
+    message: {
+      min: 'Book publisher should be at least 2 characters.',
+      max: 'Book publisher should be at most 128 characters.',
+    },
+  });
+
+  // assume authorId is valid
 };
